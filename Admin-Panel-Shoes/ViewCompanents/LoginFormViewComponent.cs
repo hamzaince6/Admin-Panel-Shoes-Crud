589ba4c6ab7a981eed3bc8ccbc47940b5b3f5f18
Hamza ince <hamzaince10@gmail.com>
2024-01-12 20:55:57 +0300

    View Component edit

---
+++ Admin-Panel-Shoes/ViewCompanents/LoginFormViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Admin_Panel_Shoes.ViewCompanents;
+
+public class LoginFormViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
---
+++ Admin-Panel-Shoes/ViewCompanents/ServicesViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Admin_Panel_Shoes.ViewCompanents;
+
+public class ServicesViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
