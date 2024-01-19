8338539549bb6cecbfc76474ab56e6c2a710a6f5
Hamza ince <hamzaince10@gmail.com>
2024-01-19 12:29:40 +0300

    admin edit customers table

--- Admin-Panel-Shoes/Controllers/AdminController.cs
+++ Admin-Panel-Shoes/Controllers/AdminController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Admin_Panel_Shoes.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Admin_Panel_Shoes.Controllers;
 
 public class AdminController : Controller
 {
+    private readonly Context _context;
+    public AdminController(Context context)
+    {
+        _context = context;
+    }
+    
     // GET
     public IActionResult Index()
     {
-        return View();
+        var deger = _context.CustomersEnumerable.ToList();
+        return View(deger);
     }
 }
