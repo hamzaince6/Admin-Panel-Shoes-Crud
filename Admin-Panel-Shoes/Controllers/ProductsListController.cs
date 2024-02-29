59ddce0d1e73a58b84cda3dd67fa26eb1dfbe35d
Muhammet Hamza İnce <hamzaince10@gmail.com>
2024-02-29 23:10:20 +0300

    database edit

---
+++ Admin-Panel-Shoes/Controllers/ProductsListController.cs
@@ -0,0 +1,20 @@
+﻿using Admin_Panel_Shoes.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Admin_Panel_Shoes.Controllers;
+
+public class CustomersListController : Controller
+{
+    private readonly Context _context;
+    public CustomersListController(Context context)
+    {
+        _context = context;
+    }
+    
+    // GET
+    public IActionResult Index()
+    {
+        var deger = _context.CustomersEnumerable.ToList();
+        return View(deger);
+    }
+}
